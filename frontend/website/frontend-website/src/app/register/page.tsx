f0b4f79490e16848d14ed4825558ffd5e760d2e5
Vitamin D <85170824+Vitamins-D@users.noreply.github.com>
2025-07-02 23:12:15 +0200

    Add code confirm

--- frontend/website/frontend-website/src/app/register/page.tsx
+++ frontend/website/frontend-website/src/app/register/page.tsx
@@ -1,48 +1,48 @@
 export default function RegisterPage() {
 return (
-    <div className="h-full bg-[url('/bg_login.png')] bg-cover bg-center bg-no-repeat">
-        <main className="flex items-center justify-center h-full">
-            <div className="bg-white rounded-2xl p-10 w-full max-w-2xl font-alexandria border-2 border-[#316BC2] min-h-[475px] text-2xl">
-                <h1 className="text-4xl text-center text-[#37A2E6] font-bold mb-6">Регистрация</h1>
+    <div className="page-wrapper">
+        <main className="main-center">
+            <div className="form-container">
+                <h1 className="heading-title">Регистрация</h1>
 
                 <form className="space-y-4">
 
                     <div>
-                        <label className="block mb-1 text-gray-700" htmlFor="username">
+                        <label className="label-style" htmlFor="username">
                             Имя пользователя
                         </label>
                         <input
                             id="username"
-                            className="w-full rounded-lg border-2 border-[#316BC2] p-3 text-xl"
+                            className="input-style"
                             placeholder="Имя пользователя"
                         />
                     </div>
 
                     <div>
-                        <label className="block mb-1 text-gray-700" htmlFor="password">
+                        <label className="label-style" htmlFor="password">
                             Пароль
                         </label>
                         <input
                             id="password"
-                            className="w-full rounded-lg border-2 border-[#316BC2] p-3 text-xl"
+                            className="input-style"
                             placeholder="Пароль"
                             type="password"
                         />
                     </div>
 
                     <div>
-                        <label className="block mb-1 text-gray-700" htmlFor="email">
+                        <label className="label-style" htmlFor="email">
                             Почта
                         </label>
                         <input
                             id="email"
-                            className="w-full rounded-lg border-2 border-[#316BC2] p-3 text-xl"
+                            className="input-style"
                             placeholder="user_email@gmail.com"
                             type="email"
                         />
                     </div>
 
-                    <div className="text-right text-xl text-gray-600 -mt-2 font-bold">
+                    <div className="link-small-note">
                         <a href="#" className="hover:underline">
                             Есть аккаунт?
                         </a>
@@ -60,7 +60,7 @@ return (
                     </div>
 
                     <button
-                        className="w-full rounded-lg bg-[#37A2E6] border-2 border-[#316BC2] text-white py-3 hover:bg-[#2d93d5] transition mt-5"
+                        className="button-primary"
                         type="submit"
                         >
                         Зарегистрироваться
